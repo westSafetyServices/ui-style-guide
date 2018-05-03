d7f8cd33e62e96efd7fda5f3c81a49381d7cfbb9
sestes <sestes@15f2d5fa-d5f1-0310-83cd-f89549c10d91>
2018-05-03 22:08:38 +0000

    WUSG-4 Updating sign in form style
    git-svn-id: https://subversion/svn/repos/wss-ui-style-guide/trunk@407297 15f2d5fa-d5f1-0310-83cd-f89549c10d91

---
+++ html/js/custom.js
@@ -0,0 +1,9 @@
+    $(function () {
+        $('#popover1').popover();
+	$('#popover2').popover();
+	$('#popover3').popover();
+	$('#popover4').popover();
+	$('#popover5').popover();
+	$('#darkPopover').popover();
+    });
+	$('[data-toggle="popover"]').popover();
